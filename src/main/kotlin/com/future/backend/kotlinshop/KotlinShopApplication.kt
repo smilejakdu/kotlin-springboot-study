881113045de51942adcc0b12877499644915a247
윤정기 <jeffrey@yunjeong-gis-MacBook-Pro.local>
2022-08-15 15:46:56 +0900

    init

---
+++ src/main/kotlin/com/future/backend/kotlinshop/KotlinShopApplication.kt
@@ -0,0 +1,11 @@
+package com.future.backend.kotlinshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/future/backend/kotlinshop/KotlinShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.future.backend.kotlinshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
